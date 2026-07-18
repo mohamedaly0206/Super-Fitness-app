a21e40040b164076bc6b2189e0fbeb1428d80a27
Khalid Adel <khalidadel209@gmail.com>
2026-07-18 15:32:41 +0300

    feat: finish login data source impl

---
+++ lib/modules/auth/data/remote/data_sources/auth_remote_data_source_contract.dart
@@ -0,0 +1,10 @@
+import 'package:super_fitness_app/config/base/base_response.dart';
+import 'package:super_fitness_app/modules/auth/data/models/request/login_response.dart';
+
+import '../../models/request/login_request_body.dart';
+
+abstract interface class AuthRemoteDataSourceContract {
+  Future<BaseResponse<LoginResponse>> login(
+      LoginRequestBody request,
+      );
+}
---
+++ lib/modules/auth/data/remote/data_sources/auth_remote_data_source_impl.dart
@@ -0,0 +1,27 @@
+import 'package:injectable/injectable.dart';
+import 'package:super_fitness_app/config/base/base_response.dart';
+import 'package:super_fitness_app/core/error/error_handler.dart';
+import 'package:super_fitness_app/modules/auth/data/models/request/login_request_body.dart';
+import 'package:super_fitness_app/modules/auth/data/models/request/login_response.dart';
+import 'package:super_fitness_app/modules/auth/data/remote/api_client/auth_api_client.dart';
+import 'package:super_fitness_app/modules/auth/data/remote/data_sources/auth_remote_data_source_contract.dart';
+
+@Injectable(as: AuthRemoteDataSourceContract)
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSourceContract {
+  final AuthApiClient _authApiClient;
+  const AuthRemoteDataSourceImpl(this._authApiClient);
+  @override
+  Future<BaseResponse<LoginResponse>> login(
+      LoginRequestBody request,
+      ) async {
+    try {
+      final response = await _authApiClient.login(request);
+
+      return SuccessBaseResponse<LoginResponse>(data: response);
+    } catch (e) {
+      return ErrorBaseResponse<LoginResponse>(
+        failure: ErrorHandler.handle(e),
+      );
+    }
+  }
+}
