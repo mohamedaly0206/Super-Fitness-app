add8424bb6ccb26f74d988cb434440c49a4770a8
Khalid Adel <khalidadel209@gmail.com>
2026-07-19 12:50:48 +0300

    feat: refactor login view

--- lib/modules/auth/presentation/login/views/login_view.dart
+++ lib/modules/auth/presentation/login/views/login_view.dart
@@ -11,7 +11,6 @@ import 'package:super_fitness_app/modules/auth/presentation/login/view_model/int
 import 'package:super_fitness_app/modules/auth/presentation/login/view_model/state/login_state.dart';
 import 'package:super_fitness_app/modules/auth/presentation/login/widgets/login_submit_button.dart';
 import 'package:super_fitness_app/modules/auth/presentation/login/widgets/social_login_buttons.dart';
-
 import '../../../../../core/layout/app_padding.dart';
 import '../../../../../core/layout/app_size.dart';
 import '../../../../../core/localization_constants/auth_constants.dart';
@@ -66,129 +65,140 @@ class _LoginViewState extends State<LoginView> {
             CustomSnackBar.success(context, 'Login Successfully!');
           }
         },
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: AppPadding.p32,
-                    horizontal: AppPadding.p16,
-                  ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: Form(
+                  key: _formKey,
                   child: Column(
+                    mainAxisSize: MainAxisSize.max,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Center(
-                        child: Image.asset(
-                          AppPng.logo,
-                          height: AppSize.s100,
-                          width: AppSize.s100,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: AppPadding.p32,
+                          horizontal: AppPadding.p16,
                         ),
-                      ),
-                      Text(
-                        AuthConstants.heyThere,
-                        style: getRegularStyle(
-                          context: context,
-                          color: AppColors.textPrimary,
-                          fontSize: FontSizeManager.s18,
-                        ),
-                      ),
-                      Text(
-                        AuthConstants.welcomeBack,
-                        style: getBoldStyle(
-                          context: context,
-                          color: AppColors.textPrimary,
-                          fontSize: FontSizeManager.s20,
-                        ),
-                      ),
-                      const AppSizedBox(height: AppSize.s0),
-                    ],
-                  ),
-                ),
-                CustomContainer(
-                  blur: 5,
-                  borderRadius: AppSize.borderRadiusLarge,
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(AppPadding.p20),
-                  child: Column(
-                    children: [
-                      Text(
-                        AuthConstants.login,
-                        style: getBoldStyle(
-                          context: context,
-                          color: AppColors.textPrimary,
-                          fontSize: FontSizeManager.s24,
-                        ),
-                      ),
-                      const AppSizedBox(height: AppSize.s20),
-                      CustomTextField(
-                        prefixIcon: const Icon(Icons.email_outlined),
-                        controller: emailController,
-                        hintText: AuthConstants.email,
-                        keyboardType: TextInputType.emailAddress,
-                        validator: AppValidator.email,
-                      ),
-                      const AppSizedBox(height: AppSize.s12),
-                      CustomTextField(
-                        prefixIcon: const Icon(Icons.lock_outline),
-                        controller: passwordController,
-                        hintText: AuthConstants.password,
-                        keyboardType: TextInputType.visiblePassword,
-                        validator: AppValidator.password,
-                        isPassword: true,
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            padding: EdgeInsets.zero,
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          ),
-                          child: Text(
-                            AuthConstants.forgetPassword,
-                            style: getRegularStyle(
-                              context: context,
-                              color: AppColors.primary,
-                              fontSize: FontSizeManager.s14,
-                            ).copyWith(
-                              decoration: TextDecoration.underline,
-                              decorationColor: AppColors.primary,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Image.asset(
+                                AppPng.logo,
+                                height: AppSize.s100,
+                                width: AppSize.s100,
+                              ),
                             ),
-                          ),
+                            Text(
+                              AuthConstants.heyThere,
+                              style: getRegularStyle(
+                                context: context,
+                                color: AppColors.textPrimary,
+                                fontSize: FontSizeManager.s18,
+                              ),
+                            ),
+                            Text(
+                              AuthConstants.welcomeBack,
+                              style: getBoldStyle(
+                                context: context,
+                                color: AppColors.textPrimary,
+                                fontSize: FontSizeManager.s20,
+                              ),
+                            ),
+                            const AppSizedBox(height: AppSize.s0),
+                          ],
                         ),
                       ),
-                      const AuthOrDivider(),
-                      const AppSizedBox(
-                        height: AppSize.s10,
-                      ),
-                      SocialLoginButtons(
-                        onFacebookTap: () {},
-                        onGoogleTap: () {},
-                        onAppleTap: () {},
-                      ),
-                      const AppSizedBox(height: AppSize.s20),
-                      BlocBuilder<LoginCubit, LoginState>(
-                        buildWhen: (previous, current) => previous.loginState.isLoading != current.loginState.isLoading,
-                        builder: (context, state) {
-                          return LoginSubmitButton(
-                            isLoading: state.loginState.isLoading,
-                            onTap: _onLoginPressed,
-                          );
-                        },
-                      ),
-                      const AppSizedBox(height: AppSize.s20),
-                      LoginRedirectRow(
-                        onLoginTap: () {},
+                      CustomContainer(
+                        blur: 5,
+                        borderRadius: AppSize.borderRadiusLarge,
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(AppPadding.p20),
+                        child: Column(
+                          children: [
+                            Text(
+                              AuthConstants.login,
+                              style: getBoldStyle(
+                                context: context,
+                                color: AppColors.textPrimary,
+                                fontSize: FontSizeManager.s24,
+                              ),
+                            ),
+                            const AppSizedBox(height: AppSize.s20),
+                            CustomTextField(
+                              prefixIcon: const Icon(Icons.email_outlined),
+                              controller: emailController,
+                              hintText: AuthConstants.email,
+                              keyboardType: TextInputType.emailAddress,
+                              validator: AppValidator.email,
+                            ),
+                            const AppSizedBox(height: AppSize.s12),
+                            CustomTextField(
+                              prefixIcon: const Icon(Icons.lock_outline),
+                              controller: passwordController,
+                              hintText: AuthConstants.password,
+                              keyboardType: TextInputType.visiblePassword,
+                              validator: AppValidator.password,
+                              isPassword: true,
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                onPressed: () {},
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                ),
+                                child: Text(
+                                  AuthConstants.forgetPassword,
+                                  style: getRegularStyle(
+                                    context: context,
+                                    color: AppColors.primary,
+                                    fontSize: FontSizeManager.s14,
+                                  ).copyWith(
+                                    decoration: TextDecoration.underline,
+                                    decorationColor: AppColors.primary,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const AuthOrDivider(),
+                            const AppSizedBox(
+                              height: AppSize.s10,
+                            ),
+                            SocialLoginButtons(
+                              onFacebookTap: () {},
+                              onGoogleTap: () {},
+                              onAppleTap: () {},
+                            ),
+                            const AppSizedBox(height: AppSize.s20),
+                            BlocBuilder<LoginCubit, LoginState>(
+                              buildWhen: (previous, current) => previous.loginState.isLoading != current.loginState.isLoading,
+                              builder: (context, state) {
+                                return LoginSubmitButton(
+                                  isLoading: state.loginState.isLoading,
+                                  onTap: _onLoginPressed,
+                                );
+                              },
+                            ),
+                            const AppSizedBox(height: AppSize.s20),
+                            LoginRedirectRow(
+                              onLoginTap: () {},
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
