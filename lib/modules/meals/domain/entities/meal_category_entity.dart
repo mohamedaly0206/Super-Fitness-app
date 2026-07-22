d98c2c57394b54bd63f022ee9344c6b29959c239
yassmena <>
2026-07-23 01:06:36 +0300

    feat: add entities for meal categories, details, and ingredients, and update repository interface for meal data fetching

---
+++ lib/modules/meals/domain/entities/meal_category_entity.dart
@@ -0,0 +1,13 @@
+class MealCategoryEntity {
+  final String id;
+  final String name;
+  final String thumbnail;
+  final String description;
+
+  const MealCategoryEntity({
+    required this.id,
+    required this.name,
+    required this.thumbnail,
+    required this.description,
+  });
+}
