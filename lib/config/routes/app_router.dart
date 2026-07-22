43d8c3427fcfe47dfb055ce7f6eddb9d2c46a011
yassmena <>
2026-07-23 01:04:03 +0300

    feat: implement food recommendation feature with API integration and routing

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:super_fitness_app/config/routes/page_transitions.dart';
 import 'package:super_fitness_app/core/widgets/not_found_screen.dart';
 import 'package:super_fitness_app/core/widgets/ui_showcase_page.dart';
+import 'package:super_fitness_app/modules/meals/presentation/food_recommendation/page/food_recommendation_page.dart';
 
 abstract class Routes {
   static const String splash = '/';
   static const String uiShowcase = '/ui-showcase';
+  static const String foodRecommendation = '/food-recommendation';
 }
 
 abstract class AppRouter {
@@ -14,6 +16,8 @@ abstract class AppRouter {
       switch (settings.name) {
         case Routes.uiShowcase:
           return PageTransitions.fade(const UIShowcasePage());
+        case Routes.foodRecommendation:
+          return PageTransitions.slide(const FoodRecommendationPage());
         default:
           return PageTransitions.fade(
             NotFoundScreen(route: settings.name ?? ''),
--- lib/modules/meals/data/datasources/meals_remote_data_source.dart
+++ lib/modules/meals/data/datasources/meals_remote_data_source.dart
@@ -1,3 +1,12 @@
+import 'package:super_fitness_app/config/base/base_response.dart';
+import 'package:super_fitness_app/modules/meals/domain/entities/meal_category_entity.dart';
+import 'package:super_fitness_app/modules/meals/domain/entities/meal_details_entity.dart';
+import 'package:super_fitness_app/modules/meals/domain/entities/meal_entity.dart';
+
 abstract class MealsRemoteDataSource {
-  // TODO: Implement
+  Future<BaseResponse<List<MealCategoryEntity>>> getCategories();
+  Future<BaseResponse<List<MealEntity>>> getMealsByCategory({
+    required String category,
+  });
+  Future<BaseResponse<MealDetailsEntity>> getMealDetails({required String id});
 }
