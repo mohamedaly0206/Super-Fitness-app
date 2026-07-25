0538e934a54a97d23375cdd6943462bb61cc90fc
Omar Ameen <omarxwork10@gmail.com>
2026-07-25 09:10:38 +0300

    refactor: add youtube extension and update routes

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:super_fitness_app/config/routes/page_transitions.dart';
 import 'package:super_fitness_app/core/widgets/not_found_screen.dart';
-import 'package:super_fitness_app/core/widgets/ui_showcase_page.dart';
+import 'package:super_fitness_app/modules/exercise/presentation/exercise/page/exercise_screen.dart';
 
 abstract class Routes {
   static const String splash = '/';
-  static const String uiShowcase = '/ui-showcase';
+  static const String exercises = '/exercises';
 }
 
 abstract class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     try {
       switch (settings.name) {
-        case Routes.uiShowcase:
-          return PageTransitions.fade(const UIShowcasePage());
+        case Routes.exercises:
+          return PageTransitions.fade(
+            ExerciseScreen(primeMoverMuscleId: '69d982ef85f6bfa972bf2248'),
+          );
         default:
           return PageTransitions.fade(
             NotFoundScreen(route: settings.name ?? ''),
