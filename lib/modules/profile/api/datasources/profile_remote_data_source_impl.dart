133167aa474410a4b6ac8c16694c18ee1f167786
Ziad Sleem <ziad.sleem.111@gmail.com>
2026-07-26 21:59:03 +0300

    feat: add profile module with API client, data sources, and repository implementation

---
+++ lib/modules/profile/api/datasources/profile_remote_data_source_impl.dart
@@ -0,0 +1,7 @@
+import 'package:injectable/injectable.dart';
+import '../../data/datasources/profile_remote_data_source.dart';
+
+@LazySingleton(as: ProfileRemoteDataSource)
+class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
+  // TODO: Implement
+}
---
+++ lib/modules/profile/data/repositories/profile_repo_impl.dart
@@ -0,0 +1,7 @@
+import 'package:injectable/injectable.dart';
+import '../../domain/repositories/profile_repo.dart';
+
+@Injectable(as: ProfileRepo)
+class ProfileRepoImpl implements ProfileRepo {
+  // TODO: Implement
+}
